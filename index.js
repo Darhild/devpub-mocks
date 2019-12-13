93a1d3df4336182c32057bb9092bb4838f9a2cd2
Исаева Мария <isaeva-mariya@yandex.ru>
2019-12-13 18:00:07 +0300

    added mocks

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const post = require('./post.json');
+const tag = require('./tag.json');
+const app = express();
+const { PORT } = require('./env');
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.use(express.static(path.resolve(__dirname, "../client/dist")));
+
+app.get("*", function(req, res) {
+  response.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
+});
+
+app.get('/api/post', (req, res) => {
+    res.send(post);
+});
+
+app.get('/api/tag', (req, res) => {
+  res.send(tag.tags);
+})
+
+app.listen(process.env.PORT || PORT);
+console.log(`Server listens on Port ${PORT}`)
