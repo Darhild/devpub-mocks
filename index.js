839cf31fc2e03109b975b95c22ae73a27e538737
Исаева Мария <isaeva-mariya@yandex.ru>
2019-12-20 11:42:49 +0300

    added cross-domain

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ const moderated = require('./moderated.json');
 const app = express();
 const { PORT } = require('./env');
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
-});
+}
+
+app.use(allowCrossDomain);
 
 app.use(express.static(path.resolve(__dirname, "../client/dist")));
 
