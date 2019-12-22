dfd50818c9df36d1239b1ce2fbebc308dcf3e4b2
Darhi <isaeva-mariya@yandex.ru>
2019-12-22 20:18:55 +0300

    added responses

--- index.js
+++ index.js
@@ -22,22 +22,62 @@ app.use(express.static(path.resolve(__dirname, "../client/dist")));
 
 app.get('/api/post', (req, res) => {
     res.send(post);
-});
+})
+
+app.get('/api/post/search', (req, res) => {
+  res.send(post);
+})
+
+app.get('/api/post/byTag', (req, res) => {
+  res.send(post);
+})
 
 app.get('/api/post/byDate', (req, res) => {
   res.send(byDate);
-});
+})
 
 app.get('/api/calendar', (req, res) => {
   res.send(calendar);
-});
+})
+
+app.get('/api/post/my', (req, res) => {
+  res.send(moderated);
+})
 
 app.get('/api/post/moderation', (req, res) => {
   res.send(moderated);
-});
+})
+
+app.get('/api/statistics/my', (req, res) => {
+  res.send({
+    "postsCount": 7,
+    "likesCount": 15,
+    "dislikesCount": 2,
+    "viewsCount": 58,
+    "firstPublication": "2018-07-16 17:35"
+  });
+})
+
+app.get('/api/statistics/all', (req, res) => {
+  res.send({
+    "postsCount": 514,
+    "likesCount": 8560,
+    "dislikesCount": 920,
+    "viewsCount": 11635,
+    "firstPublication": "2018-01-01 10:20"
+  });
+})
 
 app.get('/api/tag', (req, res) => {
   res.send(tag);
+})
+
+app.get('/api/auth/logout', (req, res) => {
+  res.send({ result: true })
+})
+
+app.post('/api/comment', (req, res) => {
+  res.send({ id: 388 })
 })
 
 app.post('/api/auth/auth', (req, res) => {
@@ -45,7 +85,74 @@ app.post('/api/auth/auth', (req, res) => {
     "result": true,
     "user": user
   });
-});
+})
+
+app.post('/api/auth/restore', (req, res) => {
+  const res = Math.round(Math.random());
+  res.send({ result: res })
+})
+
+app.post('/api/auth/like', (req, res) => {
+  res.send({ result: true })
+})
+
+app.post('/api/auth/dislike', (req, res) => {
+  res.send({ result: true })
+})
+
+app.post('/api/auth/password', (req, res) => {
+  const result = Math.round(Math.random());
+
+  if(result) {
+    res.send({ result })
+  } else res.send({
+    "result": false,
+    "errors": {
+      "code": "Ссылка для восстановления пароля устарела. <a href='login/resstore'>Запросить ссылкеу снова.</a>",
+      "captcha": "Пароль короче шести символов"
+    }
+  })
+})
+
+app.post('/api/auth/register', (req, res) => {
+  const result = Math.round(Math.random());
+
+  if(result) {
+    res.send({ result })
+  } else res.send({
+    "result": false,
+    "errors": {
+      "email": "Этот email уже зарегистрирован",
+      "captcha": "Пароль короче шести символов"
+    }
+  })
+})
+
+app.post('/api/profile/my', (req, res) => {
+  const result = Math.round(Math.random());
+
+  if(result) {
+    res.send({ result })
+  } else res.send({
+    "result": false,
+    "errors": {
+      "email": "Этот email уже зарегистрирован",
+      "photo": "Фото слишком большое, нужно не более 5 Мб"
+    }
+  })
+})
+
+app.post('/api/post', (req, res) => {
+  res.send({ result: true })
+})
+
+app.put('/api/settings', (req, res) => {
+  res.send({ result: true })
+})
+
+app.put('/api/post/:id', (req, res) => {
+  res.send({ result: true })
+})
 
 app.listen(process.env.PORT || PORT);
 console.log(`Server listens on Port ${PORT}`)
